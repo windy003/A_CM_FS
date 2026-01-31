08f7241e7187aa72634de7dbe9868719aaebdfa7
windy003 <928920471@qq.com>
2026-01-31 13:12:39 +0800

    修复了版本号

--- clashmeta_FS/app/build.gradle.kts
+++ clashmeta_FS/app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "2026/1/30-3"
+        versionName = "2026/1/31-1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
